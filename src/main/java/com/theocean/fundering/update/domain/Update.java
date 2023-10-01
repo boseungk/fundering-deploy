68bb113f68b2a528b30013209da6d3f953875bba
CheongEun <ce090404@naver.com>
2023-10-01 18:14:27 +0900

    feat: update domain entity

---
+++ src/main/java/com/theocean/fundering/update/domain/Update.java
@@ -0,0 +1,61 @@
+package com.theocean.fundering.update.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.theocean.fundering.user.domain.User;
+import com.theocean.fundering.post.domain.Post;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "updates")
+public class Update {
+    @Id
+    @Column(name = "update_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long updateId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "order")
+    private int order;
+
+    public Update(Post post, User user, String title, String content) {
+        this.post = post;
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Update update)) return false;
+        return Objects.equals(updateId, update.updateId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(updateId);
+    }
+}
