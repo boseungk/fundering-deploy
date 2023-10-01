a5d6f9f13b387c800789f0174b06e746b0043ae9
CheongEun <ce090404@naver.com>
2023-10-01 18:12:55 +0900

    feat: account domain entity

---
+++ src/main/java/com/theocean/fundering/account/domain/Account.java
@@ -0,0 +1,46 @@
+package com.theocean.fundering.account.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "account")
+@Entity
+public class Account {
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "funding_amount", nullable = false)
+    private int fundingAmount;
+
+    @Builder
+    public Account(int fundingAmount) {
+        this.fundingAmount = fundingAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(accountId, account.accountId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId);
+    }
+}
