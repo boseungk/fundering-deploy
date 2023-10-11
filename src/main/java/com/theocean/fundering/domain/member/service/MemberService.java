4c94ec47cc733d5040c33c0b35905fba90ff8c73
BoSeung <gda05189@gmail.com>
2023-10-11 14:11:22 +0900

    feat: 이메일 유효성 검사 기능
    - 이메일 유효성 검사 기능 추가 했습니다.

--- src/main/java/com/theocean/fundering/domain/member/service/MemberService.java
+++ src/main/java/com/theocean/fundering/domain/member/service/MemberService.java
@@ -1,12 +1,17 @@
 package com.theocean.fundering.domain.member.service;
 
+import com.theocean.fundering.domain.member.domain.Member;
+import com.theocean.fundering.domain.member.dto.EmailRequestDTO;
 import com.theocean.fundering.domain.member.repository.MemberRepository;
 import com.theocean.fundering.domain.member.dto.MemberRequestDTO;
+import com.theocean.fundering.global.errors.exception.Exception400;
 import com.theocean.fundering.global.errors.exception.Exception500;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -17,6 +22,7 @@ public class MemberService {
 
     @Transactional
     public void signUp(MemberRequestDTO requestDTO) {
+        sameCheckEmail(requestDTO.getEmail());
         requestDTO.encodePassword(passwordEncoder.encode(requestDTO.getPassword()));
         try {
             memberRepository.save(requestDTO.getEntity());
@@ -25,6 +31,15 @@ public class MemberService {
         }
     }
 
+    public void sameCheckEmail(String email) {
+        memberRepository.findByEmail(email)
+                .ifPresent(n -> {
+                    throw new Exception400("이메일이 존재합니다.");
+                });
+
+    }
+
     public void login(MemberRequestDTO requestDTO) {
     }
+
 }
