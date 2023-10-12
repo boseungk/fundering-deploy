e309caf216dd9df512eba7e2625d49a5eab43291
46112 <duf325@naver.com>
2023-10-13 02:55:10 +0900

    feat: CommentRepository 수정
    - 사용되지 않는 메서드들 제거
    - countByParentCommentId 추가

--- src/main/java/com/theocean/fundering/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/theocean/fundering/domain/comment/repository/CommentRepository.java
@@ -2,25 +2,9 @@ package com.theocean.fundering.domain.comment.repository;
 
 import com.theocean.fundering.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-
-import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT COALESCE(MAX(c.commentOrder), 0) FROM Comment c WHERE c.postId = :postId")
-    Long getLastCommentOrder(@Param("postId") Long postId);
-
-    Optional<Comment> findByPostIdAndCommentOrder(Long postId, Long commentOrder);
-
-    @Query("SELECT c FROM Comment c WHERE c.postId = :postId ORDER BY c.parentCommentOrder, c.commentOrder")
-    Page<Comment> findByPostIdOrdered(Long postId, Pageable pageable);
-
-    boolean existsByPostIdAndCommentOrder(Long postId, Long commentOrder);
-
-
+    long countByParentCommentId(Long parentCommentId);
 }
