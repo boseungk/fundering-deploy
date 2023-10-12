3aa5d96c403415b73082885ff0e6fdce225f9567
46112 <duf325@naver.com>
2023-10-13 00:22:40 +0900

    refactor: Comment엔티티의 order필드값 제거
    - parentCommentOrder 대신 parentCommentId(PK값) 이용
    - hasReply를 isReply로 변경하여 의미를 명확히함
    - 사용되지 않는 childCommentCount필드 제거

--- src/main/java/com/theocean/fundering/domain/comment/domain/Comment.java
+++ src/main/java/com/theocean/fundering/domain/comment/domain/Comment.java
@@ -1,6 +1,5 @@
 package com.theocean.fundering.domain.comment.domain;
 
-
 import com.theocean.fundering.global.utils.AuditingFields;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -8,7 +7,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Objects;
@@ -17,75 +15,42 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name="comment")
+@Table(name = "comment")
 @SQLDelete(sql = "UPDATE comment SET is_deleted = true WHERE comment_id = ?")
 public class Comment extends AuditingFields {
 
-    // PK
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
-    // 작성자의 식별자
     @Column(nullable = false)
     private Long writerId;
 
-    // 게시물의 식별자
     @Column(nullable = false)
     private Long postId;
 
-    // 댓글 내용
     @Column(nullable = false)
     private String content;
 
-    // 삭제 여부
     @Column(nullable = false)
     private boolean isDeleted;
 
-    // 대댓글 유무 - true이면 답글이 존재하므로 대댓글임을 의미함
     @Column(nullable = false)
-    private boolean hasReply;
+    private boolean isReply;
 
-    /*
-        commentOrder는 댓글이 생성될 때 부여받는 순서이다.
-        Comment에서는 부모댓글의 PK를 필드값으로 갖는 대신에 부모 댓글 순서를 필드값으로 가지고 있는다.
-        만약 부모댓글이 없을 경우 자기자신의 commentOrder를 parentCommentOrder필드값으로 갖는다.
-    */
+    // 부모 댓글의 ID
     @Column
-    private Long parentCommentOrder;
-
-    @Column(nullable = false)
-    private Long commentOrder;
-
-    // 대댓글 수
-    @Column(nullable = false)
-    private int childCommentCount;
+    private Long parentCommentId;
 
     @Builder
-    public Comment(Long writerId, Long postId, String content, Long commentOrder) {
+    public Comment(Long writerId, Long postId, String content, boolean isReply, Long parentCommentId) {
         this.writerId = writerId;
         this.postId = postId;
         this.content = content;
-        this.commentOrder = commentOrder;
-        this.hasReply = false;
-        this.parentCommentOrder = null;
-        this.childCommentCount = 0;
+        this.isReply = isReply;
+        this.parentCommentId = parentCommentId;
         this.isDeleted = false;
     }
-
-
-    public void updatehasReply(Boolean hasReply) {
-        this.hasReply = hasReply;
-    }
-
-    public void updateParentCommentOrder(Long parentCommentOrder) {
-        this.parentCommentOrder = parentCommentOrder;
-    }
-
-    public void increaseChildCommentCount() {
-        this.childCommentCount++;
-    }
-
 
     @Override
     public boolean equals(Object o) {
@@ -98,5 +63,4 @@ public class Comment extends AuditingFields {
     public int hashCode() {
         return Objects.hash(commentId);
     }
-
 }
