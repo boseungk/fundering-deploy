859db894bed04486aa3fc5cbf9be722b0f9cc542
46112 <duf325@naver.com>
2023-10-13 02:53:32 +0900

    feat: 댓글 조회 DTO 수정
    - parentCommentId, isReply 추가
    - order 제거

--- src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
@@ -1,24 +1,25 @@
 package com.theocean.fundering.domain.comment.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.theocean.fundering.domain.comment.domain.Comment;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 
 public class CommentResponse {
 
+    // 전체 댓글 조회 DTO
     @Getter
     public static class findAllDTO {
         private final List<commentsDTO> comments;
-
-        @JsonProperty("isLastPage")
+        private final Long lastComment;
         private final boolean isLastPage;
 
-        public findAllDTO(List<commentsDTO> comments, boolean isLastPage) {
+        public findAllDTO(List<commentsDTO> comments, boolean isLastPage, Long lastComment) {
             this.comments = comments;
+            this.lastComment = lastComment;
             this.isLastPage = isLastPage;
         }
 
@@ -26,35 +27,40 @@ public class CommentResponse {
             return isLastPage;
         }
     }
+
+    // findAllDTO의 내부에 들어갈 댓글 정보 DTO
     @Getter
     public static class commentsDTO {
+        private final Long parentComment;
         private final Long commentId;
         private final Long writerId;
         private final String writerName;
         private final String writerProfile;
         private final String content;
-        private final Long parentCommentOrder;
-        private final Long commentOrder;
-
-
-        @JsonProperty("isDeleted")
+        private final boolean isReply;
         private final boolean isDeleted;
-        private final LocalDateTime createdAt;
+        private final long createdAt;
 
         public commentsDTO(Comment comment, String writerName, String writerProfile) {
+            this.parentComment = comment.getParentCommentId();
             this.commentId = comment.getCommentId();
             this.writerId = comment.getWriterId();
             this.writerName = writerName;
             this.writerProfile = writerProfile;
             this.content = comment.getContent();
-            this.parentCommentOrder = comment.getParentCommentOrder();
-            this.commentOrder = comment.getCommentOrder();
-            this.isDeleted = comment.isDeleted();
-            this.createdAt = comment.getCreatedAt();
+            this.isReply = comment.getIsReply();
+            this.isDeleted = comment.getIsDeleted();
+            this.createdAt = toEpochSecond(comment.getCreatedAt());
         }
 
+        private long toEpochSecond(LocalDateTime localDateTime) {
+            return localDateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+        }
         public boolean getIsDeleted() {
             return isDeleted;
+        }
+        public boolean getIsReply() {
+            return isReply;
         }
     }
 }
