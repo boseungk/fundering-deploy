2b4d2ea08b494f614f57fed017e21776f6ed6f79
unknown <182876@jnu.ac.kr>
2023-10-14 22:54:00 +0900

    refactor: post에서 프ë의¸ ì²í 프론트에서 처리할 부분 제거

--- src/main/java/com/theocean/fundering/domain/post/dto/PostResponse.java
+++ src/main/java/com/theocean/fundering/domain/post/dto/PostResponse.java
@@ -27,8 +27,6 @@ public class PostResponse {
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
         private int participant;
-        private double progress;
-        private int difference;
 
 
         public FindByPostIdDTO(Post post){
@@ -45,8 +43,6 @@ public class PostResponse {
             this.createdAt = post.getCreatedAt();
             this.modifiedAt = post.getModifiedAt();
             this.participant = post.getParticipants();
-            this.progress = ((double) post.getTargetPrice() / post.getAccount().getFundingAmount()) * 100;
-            this.difference = post.getTargetPrice() - post.getAccount().getFundingAmount();
         }
     }
 
@@ -62,7 +58,6 @@ public class PostResponse {
         private String title;
         private String thumbnail;
         private int targetPrice;
-        private double progress;
         private LocalDateTime deadline;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
@@ -76,7 +71,6 @@ public class PostResponse {
             this.title = post.getTitle();
             this.thumbnail = post.getThumbnail();
             this.targetPrice = post.getTargetPrice();
-            this.progress = ((double) post.getTargetPrice() / post.getAccount().getFundingAmount()) * 100;
             this.deadline = post.getDeadline();
             this.createdAt = post.getCreatedAt();
             this.modifiedAt = post.getModifiedAt();
