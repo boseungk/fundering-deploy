e1e2f63e77b84db06537938d0cc54f6104e3cf55
46112 <duf325@naver.com>
2023-10-20 21:22:18 +0900

    style: 불필요한 주석 제거

--- src/main/java/com/theocean/fundering/domain/comment/service/CommentService.java
+++ src/main/java/com/theocean/fundering/domain/comment/service/CommentService.java
@@ -29,7 +29,9 @@ public class CommentService {
   private final PostRepository postRepository;
   private static final int REPLY_LIMIT = 30;
 
-  // (기능) 댓글 작성
+  /**
+   * (기능) 댓글 작성
+   */
   @Transactional
   public void createComment(
       final Long memberId, final Long postId, final CommentRequest.saveDTO request) {
@@ -45,17 +47,20 @@ public class CommentService {
     else createChildComment(postId, parentCommentOrder, newComment);
   }
 
+  // 작성자와 게시글 존재 유무 체크
   private void validateMemberAndPost(final Long memberId, final Long postId) {
     if (!memberRepository.existsById(memberId))
       throw new Exception404("존재하지 않는 회원입니다: " + memberId);
 
-    if (!postRepository.existsById(postId)) throw new Exception404("해당 게시글을 찾을 수 없습니다: " + postId);
+    validatePostExistence(postId);
   }
 
+  // 기본 댓글 객체 생성
   private Comment buildBaseComment(final Long memberId, final Long postId, final String content) {
     return Comment.builder().writerId(memberId).postId(postId).content(content).build();
   }
 
+  // 원댓글 생성
   private void createParentComment(final Long postId, final Comment newComment) {
     final String maxCommentOrder = commentRepository.findMaxCommentOrder(postId);
     final int newCommentOrder;
@@ -71,6 +76,7 @@ public class CommentService {
     commentRepository.save(newComment);
   }
 
+  // 대댓글 생성
   private void createChildComment(
       final Long postId, final String parentCommentOrder, final Comment newComment) {
 
@@ -78,31 +84,28 @@ public class CommentService {
       throw new Exception400("대댓글에는 댓글을 달 수 없습니다.");
     }
 
-    // DB에서 원댓글을 찾습니다
     final Comment parentComment =
         commentRepository
             .getComment(postId, parentCommentOrder)
             .orElseThrow(() -> new Exception400("원댓글을 찾을 수 없습니다."));
 
-    // 대댓글 수가 제한을 초과한 경우 댓글을 작성할 수 없다
     final int replyCount = commentRepository.countReplies(postId, parentCommentOrder + "%") - 1;
     if (REPLY_LIMIT <= replyCount) throw new Exception400("더 이상 대댓글을 달 수 없습니다.");
 
-    // commentOrder 정하기
     final String newCommentOrder = parentCommentOrder + "." + (replyCount + 1);
 
     newComment.updateCommentOrder(newCommentOrder);
     commentRepository.save(newComment);
   }
 
-  // (기능) 댓글 목록 조회 - 컨트롤러로 findAllDTO 리턴
+  /**
+   * (기능) 댓글 목록 조회
+   */
   public CommentResponse.findAllDTO getComments(
       final long postId, final String cursor, final int pageSize) {
 
-    // 1. 게시글 존재 여부 판별
     validatePostExistence(postId);
 
-    // 2. 댓글 조회 - postId가 일치하는 댓글 중 cursor보다 commentOrder값이 큰 댓글들을 pageSize+1개 가져온다
     List<Comment> comments;
     try {
       comments = customCommentRepository.getCommentList(postId, cursor, pageSize + 1);
@@ -110,16 +113,12 @@ public class CommentService {
       throw new Exception500("댓글 조회 도중 문제가 발생했습니다.");
     }
 
-    // 3. findAllDTO의 isLastPage - pageSize와 댓글 수가 일치할 때를 대비해 위에서 하나 더 조회한 상태이다
     final boolean isLastPage = comments.size() <= pageSize;
 
-    // 4. 마지막 페이지가 아닐 때는 pageSize만큼의 댓글만 사용한다
     if (!isLastPage) comments = comments.subList(0, pageSize);
 
-    // 5. findAllDTO의 comments
     final var commentsDTOs = convertToCommentDTOs(comments);
 
-    // 6. findAllDTO의 lastCursor
     String lastCursor = null;
 
     if (!comments.isEmpty()) {
@@ -139,6 +138,7 @@ public class CommentService {
     return new CommentResponse.commentDTO(comment, writer.getNickname(), writer.getProfileImage());
   }
 
+  // 게시글 존재 유무 체크
   private void validatePostExistence(final long postId) {
     if (!postRepository.existsById(postId)) {
       throw new Exception404("해당 게시글을 찾을 수 없습니다: " + postId);
@@ -150,7 +150,9 @@ public class CommentService {
     return comments.stream().map(this::createCommentsDTO).collect(Collectors.toList());
   }
 
-  // (기능) 댓글 삭제
+  /**
+   * (기능) 댓글 삭제
+   */
   @Transactional
   public void deleteComment(final Long memberId, final Long postId, final Long commentId)
       throws RuntimeException {
@@ -159,10 +161,8 @@ public class CommentService {
             .findById(commentId)
             .orElseThrow(() -> new Exception404("존재하지 않는 댓글입니다: " + commentId));
 
-    // 1. 게시글 존재 여부 확인
     if (!postRepository.existsById(postId)) throw new Exception404("해당 게시글을 찾을 수 없습니다: " + postId);
 
-    // 2. 권한 확인
     if (!memberId.equals(comment.getWriterId())) throw new Exception403("댓글 삭제 권한이 없습니다.");
 
     commentRepository.delete(comment);
